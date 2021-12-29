0f0601dc3b64c7325e469f4752a81f33a8229c0a
Eliz39 <liza.nikitina9898@gmail.com>
2021-12-30 00:00:38 +0100

    feat; add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+        let a = [
+          '', 'one', 'two', 'three', 'four',
+          'five', 'six', 'seven', 'eight', 'nine',
+          'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+          'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+        ];
+        let b = [
+          '', '', 'twenty', 'thirty', 'forty',
+          'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+        ];
+        let g = [
+          '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
+          'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
+        ];
+
+        const numToArr = String(number).split('').reverse();
+
+        if(number === 0) return 'zero';
+
+        if(number%100 === 0) {
+            return a[numToArr[2]] + ' hundred';
+        }
+
+        return [
+                  Number(numToArr[2]) === 0 ? '' : numToArr[2] ? a[numToArr[2]] + ' hundred ' : '',
+                  Number(numToArr[0]) === 0 ? b[numToArr[1]] : Number(numToArr[1]) === 1 ? '' :
+                  Number(numToArr[1]) === 0 ? '' : b[numToArr[1]] ? b[numToArr[1]] + ' ' : '',
+                  a[numToArr[1]+numToArr[0]] || a[numToArr[0]]
+                ].join('');
+};
+
